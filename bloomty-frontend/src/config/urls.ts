33a7ff66e06a3a1267b61a9ab1360596b03e514d
Sasha120797 <krutayasasha666@gmail.com>
2025-06-19 17:51:02 +0300

    Delete bloomty-frontend directory

--- bloomty-frontend/src/config/urls.ts
+++
@@ -1,5 +0,0 @@
-export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
-export const FRONTEND_URL = process.env.REACT_APP_FRONTEND_URL || 'http://localhost:3001';
-export const ADMIN_URL = process.env.REACT_APP_ADMIN_URL || 'http://localhost:3002';
-
-export const STATIC_URL = `${API_URL}/static`; 
