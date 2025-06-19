ad9acd8ab80a08cc2a21f6b3eacc523b351c02ab
Sasha120797 <krutayasasha666@gmail.com>
2025-06-19 17:51:29 +0300

    Delete bloomty-admin directory

--- bloomty-admin/src/config/urls.js
+++
@@ -1,4 +0,0 @@
-export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
-export const PRODUCTS_URL = `${API_URL}/product`;
-export const ORDERS_URL = `${API_URL}/order`;
-export const STATIC_URL = `${API_URL}/static`; 
